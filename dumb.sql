ad52971e2cb60b1b24ddd1d02b3aad4b1c9798fa
Cledson <cledson1996@gmail.com>
2023-01-22 19:58:11 -0300

    table

---
+++ dumb.sql
@@ -0,0 +1,18 @@
+CREATE TABLE "platform" (
+"id" SERIAL PRIMARY KEY,
+"name" TEXT NOT NULL,
+);
+
+CREATE TABLE "genre" (
+"id" SERIAL PRIMARY KEY,
+"name" VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE "films" (
+"id" SERIAL PRIMARY KEY,
+"name" TEXT NOT NULL,
+"genre_id" INTEGER NOT NULL REFERENCES "genre"("id"),
+"platform_id" INTEGER NOT NULL REFERENCES "platform"("id"),
+"note" TEXT,
+"status" TEXT NOT NULL
+);
