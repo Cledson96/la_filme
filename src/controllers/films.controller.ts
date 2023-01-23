9a8118fa068ca6e1c187af98d9fe3520f2a2607e
Cledson <cledson1996@gmail.com>
2023-01-22 21:03:47 -0300

    config_typescript_ok

--- src/controllers/films.controller.ts
+++ src/controllers/films.controller.ts
@@ -1,8 +1,9 @@
 import { connection } from "../database/db.js";
+import {Request,Response} from 'express'
 
-export async function films(req, res) {
+export async function films(req: Request, res: Response) {
 
-
+    
     try {
         const { rows } = await connection.query("SELECT * FROM films ;")
       res.send(rows)
